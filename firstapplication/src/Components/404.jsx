18266390e9b3ef888c9ff19f06b51b6b1960f7c8
Abdul <pc16786abdulmoiz@gmail.com>
2024-05-21 00:45:18 +0500

    Add useNavigate Hook on Counter AND 404 page

--- firstapplication/src/Components/404.jsx
+++ firstapplication/src/Components/404.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Error = () => {
+
+
+  const navigate = useNavigate();
+  
+  const shift = () => {
+    navigate("/");
+  }
+  
+  
   return (
     <>
     <h1>404 page not found ! </h1>
+    <button onClick={shift} > CLICK ME </button>
     </>
   );
 };
--- firstapplication/src/Components/counter.jsx
+++ firstapplication/src/Components/counter.jsx
@@ -1,10 +1,18 @@
 // import React from "react";
 import React ,{useEffect , useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 import "../project.css";
 
 function Counter() {
  
+const navigate  = useNavigate();
+
+function moveTo() {
+        navigate("/");
+    }
+
+
 let count =  0 ;
 
 const [currentState , UpdatedState] = useState(count);    
@@ -26,6 +34,7 @@ const reset = () =>{
         return(
         <>
         <div className="count">
+        <button onClick={moveTo}>CLICK ME </button>
         
             <h3>This is my counter</h3>
                 <button onClick={clickit}>  {currentState} </button>
